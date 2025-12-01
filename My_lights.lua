94e86fc6acfa3a60257bc9bda1bce84bf7f4bf8a
name ? <65802862+Odisey25@users.noreply.github.com>
2025-11-30 23:47:55 -0500

    Add files via upload

---
+++ My_lights.lua
@@ -0,0 +1,11 @@
+set( "sim/private/controls/lights/exponent_far", 0.52)
+set( "sim/private/controls/lights/exponent_near", 0.52)
+
+set( "sim/private/controls/lights/bloom_far", 1500)
+set( "sim/private/controls/lights/dist_far", 1000)
+set( "sim/private/controls/lights/dist_near", 100)
+set( "sim/private/controls/lights/scale_far", 0.5)
+set( "sim/private/controls/lights/scale_near", 0.5)
+set( "sim/private/controls/lights/spill_scale", 166.0)
+
+set( "sim/private/controls/lights/always_night_lights", 1)
